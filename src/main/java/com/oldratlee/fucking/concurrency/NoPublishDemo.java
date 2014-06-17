6f9e320690a84b4ffe4cd911431a6804ef07ed6b
Jerry Lee <oldratlee@gmail.com>
2014-06-17 16:18:59 +0800

    add info in javadoc

--- src/main/java/com/oldratlee/fucking/concurrency/NoPublishDemo.java
+++ src/main/java/com/oldratlee/fucking/concurrency/NoPublishDemo.java
@@ -3,7 +3,10 @@ package com.oldratlee.fucking.concurrency;
 import com.oldratlee.fucking.concurrency.util.Utils;
 
 /**
+ * <a href="http://hllvm.group.iteye.com/group/topic/34932">请问R大 有没有什么工具可以查看正在运行的类的c/汇编代码</a>提到了<b>代码提升</b>的问题。
+ *
  * @author ding.lid
+ * @see <a href="http://hllvm.group.iteye.com/group/topic/34932">请问R大 有没有什么工具可以查看正在运行的类的c/汇编代码</a>
  */
 public class NoPublishDemo {
     boolean stop = false;
