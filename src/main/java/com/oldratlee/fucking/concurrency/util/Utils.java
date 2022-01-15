26bbd64e974221b9f8cc1e0fb57cd725bd1693cd
Jerry Lee <oldratlee@gmail.com>
2022-01-16 02:53:57 +0800

    refactor LoadMaker

--- src/main/java/com/oldratlee/fucking/concurrency/util/Utils.java
+++ src/main/java/com/oldratlee/fucking/concurrency/util/Utils.java
@@ -4,22 +4,24 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * @author ding.lid
+ * @author Jerry Lee (oldratlee at gmail dot com)
  */
 public class Utils {
-    static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
-    static final int THREAD_COUNT = CPU_COUNT * 2;
-    static final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
-    static volatile boolean isLoadMade = false;
+    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+    private static final int THREAD_COUNT = CPU_COUNT * 2;
 
-    @SuppressWarnings("InfiniteLoopStatement")
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+
+    private static volatile boolean isLoadMade = false;
+
     public static synchronized void makeLoad() {
-        if (isLoadMade) return;
+        isLoadMade = true;
 
-        for (int i = 0; i < THREAD_COUNT; ++i) {
-            executorService.submit((Runnable) () -> {
-                for (int i1 = 1; ; ++i1) {
-                    if (i1 % 1000000 == 0) {
+        for (int taskCount = 0; taskCount < THREAD_COUNT; ++taskCount) {
+            executorService.submit(() -> {
+                for (int i = 1; ; ++i) {
+                    if (i % 1_000_000 == 0) {
+                        if (!isLoadMade) return;
                         sleep(1);
                     }
                 }
@@ -27,9 +29,13 @@ public class Utils {
         }
     }
 
-    public static void sleep(long l) {
+    public static void stopLoad() {
+        isLoadMade = false;
+    }
+
+    public static void sleep(long millis) {
         try {
-            Thread.sleep(l);
+            Thread.sleep(millis);
         } catch (InterruptedException ignored) {
         }
     }
